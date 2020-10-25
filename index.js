e45bd31002b497de2d3f4e3b8ad782a79d37afd5
Mosh Feu <moshfeu@gmail.com>
2020-10-25 18:12:10 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+
+
+function getHashFilter() {
+  var hash = location.hash;
+  // get filter=filterName
+  var matches = location.hash.match( /filter=([^&]+)/i );
+  var hashFilter = matches && matches[1];
+  return hashFilter && decodeURIComponent( hashFilter );
+}
+$( function() {
+
+  var $grid = $('.isotope');
+
+  // bind filter button click
+  var $filters = $('#filters').on( 'click', 'a', function() {
+    var filterAttr = $( this ).attr('data-filter');
+    console.log(filterAttr);
+    // set filter in hash
+    location.hash = 'filter=' + encodeURIComponent( filterAttr );
+  });
+
+  var isIsotopeInit = false;
+
+  function onHashchange() {
+    var hashFilter = getHashFilter();
+    if ( !hashFilter && isIsotopeInit ) {
+      return;
+    }
+    isIsotopeInit = true;
+    // filter isotope
+    $grid.isotope({
+      itemSelector: '.selector col-md-6 col-lg-4',
+      filter: hashFilter
+    });
+    // set selected class on button
+    if ( hashFilter ) {
+      $filters.find('.is-checked').removeClass('is-checked');
+      $filters.find('[data-filter="' + hashFilter + '"]').addClass('is-checked');
+    }
+  }
+
+  $(window).on( 'hashchange', onHashchange );
+
+  // trigger event handler to init Isotope
+  onHashchange();
+});
